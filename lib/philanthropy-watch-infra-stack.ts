3e54df2f8c0e008d4b3baf731dfe4bcc66bacabf
Jakob <jakob.ondrey@yello.co>
2023-02-05 13:31:17 -0600

    adds refrence to remote function

--- lib/philanthropy-watch-infra-stack.ts
+++ lib/philanthropy-watch-infra-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
-import {Rule, Schedule} from 'aws-cdk-lib/aws-events';
+import { App, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
+import { PythonFunction } from '@aws-cdk/aws-lambda-python-alpha';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
-export class PhilanthropyWatchInfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class PhilanthropyWatchInfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Rule(this, 'Daily Shout Out', {
+
+    const shoutOutFunction = new PythonFunction(this, 'Daily Shout Out Function', {
+      entry: './philanthropy-watch/daily-shoutout',
+      runtime: Runtime.PYTHON_3_9,
+      index: 'app.py',
+      handler: 'lambda_handler',
+      timeout: Duration.seconds(10),
+      memorySize: 512,
+    });
+
+
+    const shoutOutRule = new Rule(this, 'Daily Shout Out Rule', {
       schedule: Schedule.cron({
         hour: '0',
         minute: '0',
       }),
+      targets: [
+        new targets.LambdaFunction(shoutOutFunction),
+      ],
     });
   }
 }
