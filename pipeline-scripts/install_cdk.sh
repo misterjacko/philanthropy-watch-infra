4f2633c18e2c752ab6078c1cbad20bf1201eca8f
Jakob <jakob.ondrey@yello.co>
2023-02-04 21:12:51 -0600

    fix cdk version

--- pipeline-scripts/install_cdk.sh
+++ pipeline-scripts/install_cdk.sh
@@ -4,5 +4,5 @@ echo Installing TypeScript...
 npm install typescript
 
 echo Silently installing CDK...
-npm install -g aws-cdk@2.18.0 >/dev/null 2>&1
+npm install -g aws-cdk >/dev/null 2>&1
 cdk --version
